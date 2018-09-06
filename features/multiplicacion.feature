de44c288fbbc9fe50f57b53e53de83698fff3023
Leidy Gil <leidygilcha@gmail.com>
2018-09-06 16:32:13 -0500

    primeros pasos de BDD

---
+++ features/multiplicacion.feature
@@ -0,0 +1,11 @@
+#language: es
+
+Característica: multiplicación
+	Para implementar una calculadora
+	Como un desarrollador
+	Quiero saber la miltiplicación de dos números
+
+Escenario: Multiplicar dos números
+	Dado que le he introducido "5*5" en la calculadora
+	Cuando la calculadora esta ejecutando
+	Entonces la salida debería ser "25"
---
+++ features/step_definitions/calculadora_steps.rb
@@ -0,0 +1,11 @@
+Dado /^que he introducido "([^"]*)" en la calculadora$/  do |arg1|
+	pending # express the regexp above with the code you wish you had
+end
+
+Cuando /^la calculara esta ejecutando$/  do
+	pending # express the regexp above with the code you wish you had
+end
+
+Entonces /^la calculara esta ejecutando$/  do |arg1|
+	pending # express the regexp above with the code you wish you had
+end
